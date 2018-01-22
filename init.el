82614299cb4b5cd884e3f2ea26f859a7ba9c8a7d
Roger Blanco <rogerblanco10@gmail.com>
2018-01-22 14:51:39 +0000

    Removed custom lines

--- init.el
+++ init.el
@@ -143,15 +143,3 @@
 (add-hook 'sql-mode-hook
   (lambda()
     (sql-set-product "mysql")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (gruvbox-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
