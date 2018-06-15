c86f148e8c11ba1dc6829542457f1ef16bf8cd2e
Roger Blanco <rogerblanco10@gmail.com>
2018-06-15 08:49:26 +0200

    custom.el: Modificat el fitxer automaticament

--- lisp/custom.el
+++ lisp/custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit gruvbox-theme fill-column-indicator))))
+    (fill-column-indicator hungry-delete diff-hl magit web-mode php-mode gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
