f0ec68375f2e06497c71fb5a42f935aefb86cf1e
Roger Blanco <rogerblanco10@gmail.com>
2018-07-03 09:37:32 +0200

    Afegides funcions per pujar o baixar la linea

--- init.el
+++ init.el
@@ -113,6 +113,26 @@
 (global-set-key (kbd "C-c <backspace>") 'hungry-delete-backward)
 (global-set-key (kbd "C-c <deletechar>") 'hungry-delete-forward)
 
+;; NOTE(roger): Move the current line UP or DOWN
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(global-set-key [(control shift up)]  'move-line-up)
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(control shift down)]  'move-line-down)
+
 ;;; PROGRAMMING CUSTOMIZATION
 ;;; --------------------------------------------
 
